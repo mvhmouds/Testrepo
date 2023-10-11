cd1baaadda2c423f74cf09b64705124bfe23d12e
mvhmouds <mahmoud.hatramsi@gmail.com>
2023-10-11 10:21:54 -0300

    des

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-function validerEtCrypterMotDePasse($motDePasse) {
+function validerEtCrypterMotDePasse($motDePasseSaisi) {
     // Vérifier la longueur du mot de passe
-    if (strlen($motDePasse) < 6 || strlen($motDePasse) > 10) {
+    if (strlen($motDePasseSaisi) < 6 || strlen($motDePasseSaisi) > 10) {
         return "Erreur : Le mot de passe doit avoir entre 6 et 10 caractères.";
     }
-    
+
     // Créer le "salt" statique
     $salt = "ABC1234@";
 
     // Concaténer le "salt" au mot de passe
-    $motDePasseAvecSalt = $motDePasse . $salt;
+    $motDePasseAvecSalt = $motDePasseSaisi . $salt;
 
     // Chiffrer le mot de passe (vous pouvez utiliser une méthode de hachage, par exemple, MD5)
     $motDePasseCrypte = md5($motDePasseAvecSalt);
@@ -25,10 +25,17 @@ function validerEtCrypterMotDePasse($motDePasse) {
     }
 }
 
-// Exemple d'utilisation
-$motDePasseSaisi = "MonMotDePasse";
-$message = validerEtCrypterMotDePasse($motDePasseSaisi);
-echo "Salt : ABC1234@<br>";
-echo "Mot de passe chiffré : " . md5($motDePasseSaisi . "ABC1234@") . "<br>";
-echo $message;
+if (isset($_POST['motDePasse'])) {
+    $motDePasseSaisi = $_POST['motDePasse'];
+    $message = validerEtCrypterMotDePasse($motDePasseSaisi);
+    echo "Salt : ABC1234@<br>";
+    echo "Mot de passe chiffré : " . md5($motDePasseSaisi . "ABC1234@") . "<br>";
+    echo $message;
+} else {
+    // Afficher le formulaire pour saisir le mot de passe
+    echo '<form method="POST" action="">
+    Entrez le mot de passe : <input type="password" name="motDePasse">
+    <input type="submit" value="Valider">
+    </form>';
+}
 ?>
