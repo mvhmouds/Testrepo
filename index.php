d213029fd4e97d0c306fede4cc722d3bded70b7f
mvhmouds <mahmoud.hatramsi@gmail.com>
2023-10-11 10:18:49 -0300

    descp

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php
-
-// Check if the form has been submitted
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Get the password from the form
-    $motDePasseFourni = $_POST["password"];
-    
-    // Perform password validation
-    $resultatValidation = validerMotDePasse($motDePasseFourni);
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Password Validation</title>
-</head>
-<body>
-    <h1>Password Validation</h1>
-    <form method="POST" action="validate_password.php"> <!-- Modify the action attribute as needed -->
-        <label for="password">Enter Password:</label>
-        <input type="password" id="password" name="password" required>
-        <input type="submit" value="Submit">
-    </form>
-
-    <?php
-    // Display the validation result
-    if (isset($resultatValidation)) {
-        echo "<p>$resultatValidation</p>";
+function validerEtCrypterMotDePasse($motDePasse) {
+    // Vérifier la longueur du mot de passe
+    if (strlen($motDePasse) < 6 || strlen($motDePasse) > 10) {
+        return "Erreur : Le mot de passe doit avoir entre 6 et 10 caractères.";
     }
-    ?>
-</body>
-</html>
+    
+    // Créer le "salt" statique
+    $salt = "ABC1234@";
+
+    // Concaténer le "salt" au mot de passe
+    $motDePasseAvecSalt = $motDePasse . $salt;
+
+    // Chiffrer le mot de passe (vous pouvez utiliser une méthode de hachage, par exemple, MD5)
+    $motDePasseCrypte = md5($motDePasseAvecSalt);
+
+    // Simuler un mot de passe correct
+    $motDePasseCorrect = md5("MotDePasseCorrect" . $salt);
+
+    // Comparer le mot de passe entré avec le mot de passe correct
+    if ($motDePasseCrypte === $motDePasseCorrect) {
+        return "Mot de passe correct. Accès autorisé !";
+    } else {
+        return "Mot de passe incorrect. Accès refusé.";
+    }
+}
+
+// Exemple d'utilisation
+$motDePasseSaisi = "MonMotDePasse";
+$message = validerEtCrypterMotDePasse($motDePasseSaisi);
+echo "Salt : ABC1234@<br>";
+echo "Mot de passe chiffré : " . md5($motDePasseSaisi . "ABC1234@") . "<br>";
+echo $message;
+?>
