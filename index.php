df2a69a2fc2f062e5b62e03e2a9347ce38d7f025
mvhmouds <mahmoud.hatramsi@gmail.com>
2023-10-11 10:09:03 -0300

    descp

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+// Include the password validation function
+require_once('password_validation.php'); // Include the file with your validation code
+
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the password from the form
+    $motDePasseFourni = $_POST["password"];
+    
+    // Perform password validation
+    $resultatValidation = validerMotDePasse($motDePasseFourni);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Validation</title>
+</head>
+<body>
+    <h1>Password Validation</h1>
+    <form method="POST" action="validate_password.php"> <!-- Modify the action attribute as needed -->
+        <label for="password">Enter Password:</label>
+        <input type="password" id="password" name="password" required>
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    // Display the validation result
+    if (isset($resultatValidation)) {
+        echo "<p>$resultatValidation</p>";
+    }
+    ?>
+</body>
+</html>
